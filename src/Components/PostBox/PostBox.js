f8833c28653c8288b2193e693846e483d49d44fa
Lúcio Diego <81719915+SLXPrime@users.noreply.github.com>
2022-02-08 19:58:10 -0300

    subindo Git

--- src/Components/PostBox/PostBox.js
+++ src/Components/PostBox/PostBox.js
@@ -12,7 +12,7 @@ function PostBox(props) {
             {props.children}
             <div className="button-bar">
                 <div className='comment'>
-                        <a href='#'>
+                        <a href='.'>
                             <button className='Go'> <i class="fas fa-share"></i> Vá para o Git da aplicação</button>
                         </a>
                 </div>
